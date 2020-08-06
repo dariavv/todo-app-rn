9c46b9cc9339312e390d4d934c1e4c767c5b862d
dariavv <daria.sidorovich333@gmail.com>
2020-08-06 12:21:08 +0300

    FEAT: add icons and react-native-elements lib

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import THEME from 'theme';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Navbar: React.FC = () => {
   return (
     <View style={styles.navbar}>
+      <Text style={styles.icon}>
+        <Icon name="leaf-outline" size={25} color="#ffffff" />
+      </Text>
       <Text style={styles.text}>Ethereal Todo App</Text>
+      <Text style={styles.icon}>
+        <Icon name="rose-outline" size={25} color="#ffffff" />
+      </Text>
     </View>
   );
 };
@@ -13,8 +20,9 @@ const Navbar: React.FC = () => {
 const styles = StyleSheet.create({
   navbar: {
     height: 90,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
     backgroundColor: THEME.MAIN_COLOR,
     paddingBottom: 5,
   },
@@ -22,6 +30,10 @@ const styles = StyleSheet.create({
     color: THEME.WHITE_COLOR,
     fontSize: 27,
     fontFamily: 'BadScript-Regular',
+  },
+  icon: {
+    paddingVertical: 12,
+    paddingHorizontal: 9,
   },
 });
 
