2c56499e8a039061aa66ab317dfaad7c4299a7af
dariavv <daria.sidorovich333@gmail.com>
2020-08-12 21:37:11 +0300

    REFACTOR: refactoring todoReducer

--- src/context/todo/todoReducer.ts
+++ src/context/todo/todoReducer.ts
@@ -1,35 +1,52 @@
-/* eslint-disable no-param-reassign */
 import { ADD_ITEM, UPDATE_ITEM, REMOVE_ITEM } from 'types';
-import { ITodo } from 'interfases';
+import { ITodo, ITodoReducer } from 'interfases';
+
+type State = {
+  todos: [{ id: string; title: string }];
+};
+
+type Handlers = {
+  [ADD_ITEM]: (state: State, { payload }: { payload: string }) => any;
+  [UPDATE_ITEM]: (
+    state: State,
+    {
+      payload: { id, title },
+    }: any,
+  ) => any;
+  [REMOVE_ITEM]: (state: State, { payload }: { payload: string }) => any;
+
+  DEFAULT: (state: State) => any;
+};
+
+const handlers: Handlers = {
+  [ADD_ITEM]: (state, { payload }) => ({
+    ...state,
+    todos: [...state.todos, { id: Date.now().toString(), title: payload }],
+  }),
+
+  [UPDATE_ITEM]: (state, { payload: { id, title } }) => ({
+    ...state,
+    todos: state.todos.map((item: ITodoReducer) => {
+      if (item.id === id) {
+        return { ...item, title };
+      }
+      return item;
+    }),
+  }),
+
+  [REMOVE_ITEM]: (state, { payload }) => ({
+    ...state,
+    todos: state.todos.filter((item: ITodo) => item.id !== payload),
+  }),
+
+  DEFAULT: (state) => state,
+};
 
 const todoReducer = (state: any, action: any) => {
-  switch (action.type) {
-    case ADD_ITEM:
-      return {
-        ...state,
-        todos: [
-          ...state.todo,
-          { id: Date.now().toString(), title: action.title },
-        ],
-      };
-    case UPDATE_ITEM:
-      return {
-        ...state,
-        todos: state.todos.map((item: ITodo) => {
-          if (item.id === action.id) {
-            item.title = action.title;
-          }
-          return item;
-        }),
-      };
-    case REMOVE_ITEM:
-      return {
-        ...state,
-        todos: state.todos.filter((item: ITodo) => item.id !== action.id),
-      };
-    default:
-      return state;
-  }
+  const handler =
+    handlers[action.type as 'ADD_ITEM' | 'UPDATE_ITEM' | 'REMOVE_ITEM'] ||
+    handlers.DEFAULT;
+  return handler(state, action);
 };
 
 export default todoReducer;
--- src/interfases.ts
+++ src/interfases.ts
@@ -2,3 +2,8 @@ export interface ITodo {
   id: string;
   title: string;
 }
+
+export interface ITodoReducer {
+  id: string;
+  title: string | undefined;
+}
